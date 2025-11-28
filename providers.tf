d31bfd64faeb3405d1b3ecf62f3f384e1b1c4474
Eric (E-Mac) Mack <94939145+emack8258@users.noreply.github.com>
2025-11-27 23:49:34 -0800

    Update providers.tf
    Reverted to default

--- providers.tf
+++ providers.tf
@@ -1,6 +1,3 @@
-variable = "assume_role_arn" {
-  type = string
-}
 terraform {
   required_providers {
     aws = {
@@ -11,7 +8,4 @@ terraform {
 
 provider "aws" {
   region  = "us-west-2"
-    role_arn     = var.assume_role_arn
-    session_name = "github-terraform"
-    }
 }
